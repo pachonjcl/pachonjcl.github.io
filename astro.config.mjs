41bbcd15541110853bfa11cdf56d7f4e556665f0
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2024-12-08 15:14:52 -0400

    test workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,19 +17,22 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://pachonjcl.github.io";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
+let BASE = "";
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
+  BASE = "v2website";
 }
 
 export default defineConfig({
   server: { port: SERVER_PORT },
   site: BASE_URL,
+  base: BASE,
   integrations: [
     sitemap(),
     tailwind({
